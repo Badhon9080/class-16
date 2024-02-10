3fdd4c9f00d2b500e75f5d71a299f9e56e772a83
BADHAN SEN <96924254+Badhon9080@users.noreply.github.com>
2024-02-10 13:31:24 +0600

    Add files via upload

---
+++ allfoods.php
@@ -0,0 +1,13 @@
+<?php
+   include_once "./bacend-lay/header.php"
+?>
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                  allfoods
+                </div>
+                <!-- /.container-fluid -->
+<?php
+   include_once "./bacend-lay/footer.php";
+?>
